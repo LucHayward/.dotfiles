70e65c5a9e1f7509bc78be36684a92dc17929e71
Luc Hayward <luchay10@gmail.com>
2023-09-02 17:34:40 +0200

    ¯\_(ツ)_/¯ git please work

--- linux_install.sh
+++ linux_install.sh
@@ -38,6 +38,7 @@ sudo apt update
 # Install packages using apt
 sudo apt install -y \
   git \
+  gnome-tweaks
 
 # exa doesn't install correctly on ubuntu
 sudo apt install -y cargo
@@ -79,4 +80,3 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
-
