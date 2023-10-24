2d0a522bcbe96dca29fa00775bd1f01bf185b4a7
Luc Hayward <luchay10@gmail.com>
2023-10-24 10:00:05 +0200

    Standardize confirmation function in setup scripts

--- common_install.sh
+++ common_install.sh
@@ -6,59 +6,64 @@
 
 source ~/.dotfiles/define_colours.sh
 
+# Function to ask for confirmation with a description
+function ask_confirmation() {
+    local description="$1"
+    read -p "Do you want to run the following section? (y/n): $description : " choice
+    case "$choice" in
+        [Yy]* ) return 0;;
+        * ) return 1;;
+    esac
+}
+
 # Ask for the administrator password upfront
 sudo -v
 
 # ======================================
-# Symllink to various dotfiles
+# Symllink to various dotfiles and directories
 # ======================================
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+if ask_confirmation "Symmlink various dotfiles"; then
+    ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+    ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+    ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+    ln -sf ~/.dotfiles/.pandoc ~/.pandoc
 
-# =====
-# Move directories to ~/
-# =====
-mv ~/.dotfiles/.pandoc ~/.pandoc
-
-# ==============================
-# Run OS specific install script	
-# ==============================
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    *)          machine="UNKNOWN:${unameOut}"
-esac
-echo -e "Running on ${machine}"
-
-# Check the current operating system and source the appropriate script
-if [[ "${machine}" == "Mac" ]]; then
-    source mac_install.sh
-elif [[ "${machine}" == "Linux" ]]; then
-    source linux_install.sh
-else
-    echo "Operating system is not macOS or Linux. Skipping installation."
 fi
 
+# ==============================
+# Run OS specific install script    
+# ==============================
+if ask_confirmation "Run OS specific install script"; then
+    unameOut="$(uname -s)"
+    case "${unameOut}" in
+        Linux*)     machine=Linux;;
+        Darwin*)    machine=Mac;;
+        *)          machine="UNKNOWN:${unameOut}"
+    esac
+    echo -e "Running on ${machine}"
+
+    # Check the current operating system and source the appropriate script
+    if [[ "${machine}" == "Mac" ]]; then
+        source mac_install.sh
+    elif [[ "${machine}" == "Linux" ]]; then
+        source linux_install.sh
+    else
+        echo "Operating system is not macOS or Linux. Skipping installation."
+    fi
+fi
 
 # ==================================
 # Setup zsh and nice-to-have plugins
 # MacOS already has zsh installed
 # ==================================
-# Change zsh to be the default shell
-echo -ne "$BOLD Set zsh to default shell? (y/n): $RESET"
-read -p " " set_zsh
-if [[ $set_zsh == [yY] ]]; then
+if ask_confirmation "Set zsh to default shell"; then
     echo -e "$RESET[D] Sudo-ing to make zsh default shell$RESET$RED"
     sudo sh -c "echo $(which zsh) >> /etc/shells" && chsh -s $(which zsh)
     echo -e "$RESET"
 fi
 
-echo -ne "$BOLD Install zsh plugins? (y/n): $RESET"
-read -p " " set_zsh
-if [[ $set_zsh == [yY] ]]; then
+if ask_confirmation "Install zsh plugins"; then
     echo -e "$RESET[D] Installing zsh plugins$RESET$RED"
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     echo -e "$RESET"
--- mac_install.sh
+++ mac_install.sh
@@ -5,157 +5,170 @@
 # and https://github.com/beyarkay/dotfiles/
 # ==============================================================================
 
+# Function to ask for confirmation with a description
+function ask_confirmation() {
+    local description="$1"
+    read -p "Do you want to run the following section? (y/n): $description : " choice
+    case "$choice" in
+        [Yy]* ) return 0;;
+        * ) return 1;;
+    esac
+}
 
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we're about to change
-osascript -e 'tell application "System Preferences" to quit'
+# ========================
+# Set mac system settings
+# ========================
+if ask_confirmation "Set mac system settings"; then
+	# Close any open System Preferences panes, to prevent them from overriding
+	# settings we're about to change
+	osascript -e 'tell application "System Preferences" to quit'
 
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+	# Trackpad: enable tap to click for this user and for the login screen
+	defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+	defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+	defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+	defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+	# Disable the sound effects on boot
+	sudo nvram SystemAudioVolume=" "
 
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+	# Require password immediately after sleep or screen saver begins
+	defaults write com.apple.screensaver askForPassword -int 1
+	defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+	# Save screenshots to the desktop
+	defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
-# Disable shadow in screenshots
-defaults write com.apple.screencapture disable-shadow -bool true
+	# Disable shadow in screenshots
+	defaults write com.apple.screencapture disable-shadow -bool true
 
-# Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+	# Finder: show all filename extensions
+	defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder: show status bar
-defaults write com.apple.finder ShowStatusBar -bool false
+	# Finder: show status bar
+	defaults write com.apple.finder ShowStatusBar -bool false
 
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
+	# Finder: show path bar
+	defaults write com.apple.finder ShowPathbar -bool true
 
-# Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool false
+	# Display full POSIX path as Finder window title
+	defaults write com.apple.finder _FXShowPosixPathInTitle -bool false
 
-# Keep folders on top when sorting by name
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
+	# Keep folders on top when sorting by name
+	defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-# When performing a search, search the current folder by default
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+	# When performing a search, search the current folder by default
+	defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+	# Disable the warning when changing a file extension
+	defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Automatically open a new Finder window when a volume is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+	# Automatically open a new Finder window when a volume is mounted
+	defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+	defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+	defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+	# Use list view in all Finder windows by default
+	# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+	defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Show the ~/Library folder
-# chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+	# Show the ~/Library folder
+	# chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
 
-# Set the icon size of Dock items to 36 pixels
-defaults write com.apple.dock tilesize -int 36
+	# Set the icon size of Dock items to 36 pixels
+	defaults write com.apple.dock tilesize -int 36
 
-# Wipe all (default) app icons from the Dock
-# This is only really useful when setting up a new Mac, or if you don't use
-# the Dock to launch apps.
-defaults write com.apple.dock persistent-apps -array
+	# Wipe all (default) app icons from the Dock
+	# This is only really useful when setting up a new Mac, or if you don't use
+	# the Dock to launch apps.
+	defaults write com.apple.dock persistent-apps -array
 
-# Show only open applications in the Dock
-defaults write com.apple.dock static-only -bool true
+	# Show only open applications in the Dock
+	defaults write com.apple.dock static-only -bool true
 
-# Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
+	# Automatically hide and show the Dock
+	defaults write com.apple.dock autohide -bool true
 
-# Make Dock icons of hidden applications translucent
-defaults write com.apple.dock showhidden -bool true
+	# Make Dock icons of hidden applications translucent
+	defaults write com.apple.dock showhidden -bool true
 
-# Remove dock autohide delay
-defaults write com.apple.dock autohide-delay -float 0
+	# Remove dock autohide delay
+	defaults write com.apple.dock autohide-delay -float 0
 
-# Restart the System?
-/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+	# Restart the System?
+	/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 
-# Restart all the affected apps
-for app in "cfprefsd" \
-	"Dock" \
-	"Finder" \
-	"SystemUIServer" \
-	"iCal"; do
-	killall "${app}" &> /dev/null
-done
+	# Restart all the affected apps
+	for app in "cfprefsd" \
+		"Dock" \
+		"Finder" \
+		"SystemUIServer" \
+		"iCal"; do
+		killall "${app}" &> /dev/null
+	done
 
-
+fi
 
 # ================
 # Install homebrew
 # ================
-echo -ne "$BOLD Install homebrew? (y/n): $RESET"
-read -p " " install_homebrew
-if [[ $install_homebrew == [yY] ]]; then
-	echo -e "$RESET$BOLD Installing homebrew$RESET"
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	echo -e "$BOLD Homebrew installation finished$RESET"
+if ask_confirmation "Install homebrew"; then
+    echo -e "$RESET$BOLD Installing homebrew$RESET"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo -e "$BOLD Homebrew installation finished$RESET"
 fi
 
 # ================
 # Install packages
 # ================
-
-brew install \
- git \
- exa \
- miniconda \
- pandoc \
- gh
+if ask_confirmation "Install packages"; then
+    brew install \
+    git \
+    exa \
+    miniconda \
+    pandoc \
+    gh
+fi
 
 # =============
 # Install casks
 # =============
-
-brew install --cask --no-quarentine\
- whatsapp \
- telegram \
- rectangle \
- transmission \
- vlc \
- macs-fan-control \
- iterm2 \
- jetbrains-toolbox \
- Visual-studio-code \
- mactex-no-gui \
- discord \
- todoist \
- qlmarkdown \
- syntax-highlight
+if ask_confirmation "Install casks"; then
+    brew install --cask --no-quarantine \
+    whatsapp \
+    telegram \
+    rectangle \
+    transmission \
+    vlc \
+    macs-fan-control \
+    iterm2 \
+    jetbrains-toolbox \
+    visual-studio-code \
+    mactex-no-gui \
+    discord \
+    todoist \
+    qlmarkdown \
+    syntax-highlight
+fi
 
 # =================
 # Install Nerdfonts
-# https://github.com/ryanoasis/nerd-fonts/discussions/1103
-# Nerd Font Mono: Strictly monospaced (programming maybe?)
-# Nerd Font: Monospaced but potentially overlapping (terminal maybe?)
-# Nerd Font Propo: Proportional (text/writing)
 # =================
-brew tap homebrew/cask-fonts && brew install --cask  font-jetbrains-mono-nerd-font font-fira-code-nerd-font
+if ask_confirmation "Install Nerdfonts"; then
+    brew tap homebrew/cask-fonts && brew install --cask  font-jetbrains-mono-nerd-font font-fira-code-nerd-font
+fi
 
-
-
-# Install the One Dark theme for iTerm
-open "${HOME}/.dotfiles/One Dark whiter.itermcolors"
-
-# Initialise conda
-conda init "$(basename "${SHELL}")"
+# ==========================
+# Install and setup iTerm2
+# ==========================
+if ask_confirmation "Install and setup iTerm2"; then
+    open "${HOME}/.dotfiles/One Dark whiter.itermcolors"
+    conda init "$(basename "${SHELL}")"
+fi
 
 # ==========================
 # Symlink Sublim Preferences
 # ==========================
-ln -s ~/.dotfiles/Preferences.sublime_settings /Users/luchayward/Library/Application Support/Sublime Text/Packages/User/
+if ask_confirmation "Symlink Sublime Preferences"; then
+    ln -s ~/.dotfiles/Preferences.sublime_settings /Users/luchayward/Library/Application Support/Sublime Text/Packages/User/
+fi
