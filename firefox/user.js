b21c17cc5a67ca2f0b803fa94fe35d6a2591344f
Luc Hayward <luchay@amazon.com>
2026-06-18 21:31:49 +0200

    feat(firefox): Add user.js with session restore pref

---
+++ firefox/user.js
@@ -0,0 +1,8 @@
+// Firefox preferences managed by dotfiles
+// These override defaults on every Firefox startup
+
+// Enable userChrome.css customization
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Restore previous session on startup
+user_pref("browser.startup.page", 3);
